3ccc6a33d28adc3b6145922f78c3c1349ad6dee8
IAN OTOLLO <ianotollo@gmail.com>
2026-02-07 16:40:00 +0300

    Fix: restored config.php inclusions and added direct dependency on helpers.php in Auth.php to fix fatal undefined function error

--- includes/Auth.php
+++ includes/Auth.php
@@ -5,6 +5,7 @@
 
 require_once 'Database.php';
 require_once 'Session.php';
+require_once 'helpers.php';
 
 class Auth
 {
--- member-dashboard.php
+++ member-dashboard.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'config/config.php';
 require_once 'includes/Auth.php';
 require_once 'includes/Database.php';
 
